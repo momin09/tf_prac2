9e5e785f8b78ea26054f11a618d4fd4cbd2303b0
momin09 <khm0909z@gmail.com>
2023-02-06 03:23:23 +0900

    Add: Web Server instance in Private Sub

---
+++ chap6/instance_webServer.tf
@@ -0,0 +1,26 @@
+/*
+resource "aws_instance" "sample-ec2-web01" {
+    tags = {
+        Name = "sample-ec2-web01"
+    }
+
+    ami = "ami-013218fccb68a90d4"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.private01.id
+
+    key_name = aws_key_pair.exampleMomin-instance-key-pair.key_name
+}
+
+resource "aws_instance" "sample-ec2-web02" {
+    tags = {
+        Name = "sample-ec2-web02"
+    }
+
+    ami = "ami-013218fccb68a90d4"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.private02.id
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.exampleMomin-instance-key-pair.key_name
+}
+*/
